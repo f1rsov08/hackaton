e856363e9d38f9d55ff7a3eda63b02d397f7b114
Дмитрий Фирсов <firsov.2008@mail.ru>
2024-02-24 19:10:41 +0300

    кхм

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import card
 pygame.init()
 size = width, height = 800, 900
 screen = pygame.display.set_mode(size)
-card.show_card(screen, 'data/cards/1.json')
 memory.memory(screen)
 card.show_card(screen, 'data/cards/1.json')
-memory.memory(screen)
-card.show_card(screen, 'data/cards/1.json')
-memory.memory(screen)
-card.show_card(screen, 'data/cards/1.json')
-memory.memory(screen)
 pygame.quit()
