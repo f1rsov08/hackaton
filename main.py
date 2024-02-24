40482f811a22297308d22da6241da9817ba11d49
Дмитрий Фирсов <firsov.2008@mail.ru>
2024-02-24 13:27:59 +0300

    карточки с ифнормацией

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pygame
 import memory
+import card
 
 pygame.init()
 size = width, height = 800, 900
 screen = pygame.display.set_mode(size)
+card.show_card(screen, 'data/cards/1.json')
+memory.memory(screen)
+card.show_card(screen, 'data/cards/1.json')
+memory.memory(screen)
+card.show_card(screen, 'data/cards/1.json')
+memory.memory(screen)
+card.show_card(screen, 'data/cards/1.json')
 memory.memory(screen)
 pygame.quit()
