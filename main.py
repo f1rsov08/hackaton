b917544ead50f2f257737fe8a9af27d58daa8d34
Дмитрий Фирсов <firsov.2008@mail.ru>
2024-02-24 19:17:28 +0300

    ой, я чет сломал, все починил

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 import memory
+import puzzle
 import card
 
 pygame.init()
 size = width, height = 800, 900
 screen = pygame.display.set_mode(size)
 memory.memory(screen)
+card.show_card(screen, 'data/cards/1.json')
+puzzle.puzzle(screen)
 card.show_card(screen, 'data/cards/1.json')
 pygame.quit()
